778a62a6e50acd26144ca5eab8d3194c8131fe35
Fairuz <firdauz2007123@gmail.com>
2024-06-24 16:38:32 +0700

    add variebl superglobal

---
+++ variebel-superGLobal/get.php
@@ -0,0 +1,14 @@
+<?php
+    //get adalah mengirimkan suatu data yang akan di
+    // tunjukan di browser
+    
+    //cara membuat get:
+    // -bisa melalui lik localhost nya langsung
+    //  http://localhost:3000/variebel-superGLobal/get.php?umur=12
+    //    nama = 'fairuz' ==> key dan value
+    //  
+    // - atau bisa buat di vscode langsung
+    $_GET['nama'] = 'fairyz ';
+    $_GET['kelas'] = 'x';
+    var_dump($_GET)
+?>
